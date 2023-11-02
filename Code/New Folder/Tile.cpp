808e62be5a07585d43ec54726e236ceea56cc00a
u21463230 <133500463+u21463230@users.noreply.github.com>
2023-11-02 14:14:55 +0200

    Update Tile.cpp - Julia

--- Code/New Folder/Tile.cpp
+++ Code/New Folder/Tile.cpp
@@ -1,6 +1,22 @@
 #include "Tile.h"
 
-Tile::Tile() {
-	// TODO - implement Tile::Tile
-	throw "Not yet implemented";
+Tile::Tile(int x, int y) {
+    this->xDim = x;
+    this->yDim = y;
+}
+
+int Tile::getx() {
+    return this->xDim;
+}
+
+int Tile::gety() {
+    return this->yDim;
+}
+
+char Tile::getSym() {
+    return this->symbol;
+}
+
+void Tile::setSym(char s) {
+    this->symbol = s;
 }
