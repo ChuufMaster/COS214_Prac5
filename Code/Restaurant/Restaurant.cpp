ee06ae0a773f26cd34c7ef7bb51bbb239d19d32f
Janicke Jacobs <134176044+Janicke01@users.noreply.github.com>
2023-11-02 22:21:59 +0200

    Update Restaurant - Janicke.cpp

--- Code/Restaurant/Restaurant.cpp
+++ Code/Restaurant/Restaurant.cpp
@@ -1,6 +1,21 @@
 #include "Restaurant.h"
 
-void Restaurant::incOccupancy() {
+void Restaurant::incOccupancy() 
+{
 	// TODO - implement Restaurant::incOccupancy
-	throw "Not yet implemented";
+	//throw "Not yet implemented";
+	occupancy++;//added
+}
+
+Restaurant::Restaurant()
+{
+	singletonData = 0;
+}
+
+Restaurant::~Restaurant() {}
+
+Restaurant& Restaurant::_restaurant()
+{
+	static Restaurant onlyInstance_;
+	return onlyInstance_;
 }
