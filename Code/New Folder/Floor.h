53317cd068568eae6df1a81862ce04874755ec4a
u21463230 <133500463+u21463230@users.noreply.github.com>
2023-11-02 14:23:05 +0200

    Update Floor.h - Julia

--- Code/New Folder/Floor.h
+++ Code/New Folder/Floor.h
@@ -10,6 +10,7 @@ public:
     Floor(); ///Default floor plan for the entire system (adjust UML)
     void print(); ///prints the floor
     void changeTile(Tile*); /// changing of the tile at that location on the floor
+    Tile getTile(int, int);
 private:
 	std::vector<std::vector<Tile>> floor;
 };
