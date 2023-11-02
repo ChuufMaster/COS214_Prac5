c11cf14f1487f22100c4fbe4ee0753aa3ecb328d
u21463230 <133500463+u21463230@users.noreply.github.com>
2023-11-02 14:12:25 +0200

    Updating Floor.h - Julia

--- Code/New Folder/Floor.h
+++ Code/New Folder/Floor.h
@@ -1,10 +1,17 @@
 #ifndef FLOOR_H
 #define FLOOR_H
 
-class Floor {
+#include <iostream>
+#include <vector>
+#include "Tile.h"
 
+class Floor {
+public:
+    Floor(); ///Default floor plan for the entire system (adjust UML)
+    void print(); ///prints the floor
+    void changeTile(Tile*); /// changing of the tile at that location on the floor
 private:
-	vector<vector<Tile*>> _floor;
+	std::vector<std::vector<Tile>> floor;
 };
 
 #endif
