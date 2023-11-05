c547dd2a04d29a594cb22ffbc4c0de8fb4728c86
colebt <u20428091@tuks.co.za>
2023-11-05 14:16:01 +0200

    Changes to Customer constructor to include MenuItem

--- Code/Customer/NormalCustomer.cpp
+++ Code/Customer/NormalCustomer.cpp
@@ -1,6 +1,6 @@
 #include "NormalCustomer.h"
 
-NormalCustomer::NormalCustomer(int x, int y) : Customer(x, y) {
+NormalCustomer::NormalCustomer(int x, int y, MenuItem m) : Customer(x, y, m) {
   // TODO - implement NormalCustomer::NormalCustomer
   throw "Not yet implemented";
   CustomerHappy *normalState = new CustomerHappy(5);
